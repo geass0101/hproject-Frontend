01ce9fe48ec541658326df53b55f9952e0e994e2
MADISONMK\aaron.velasco <aaronvdp@hotmail.com>
2018-05-04 13:25:32 +0200

    Search completado Formulario de búsqueda en funcionamiento

--- src/components/search/search.vue
+++ src/components/search/search.vue
@@ -1,27 +1,29 @@
 <template>
   <div>
-    <div class="layout-view">
+    <div class="layout-view layout-padding">
       <div class="text-center">
           <h1>Buscar</h1>
+            <q-search v-model="srch" v-on:change="search" />
       </div>
 
       <div class="layout-view layout-padding">
-
-      <q-search v-model="srch" />
-      <q-btn color="secondary"  @click='search'>Buscar</q-btn>
+        <q-list striped-odd sparse no-border>
       <q-item v-for="user in results" :key="user.id">
         <q-item-main>
           <q-item-tile label>{{ user.name }}
+            <q-btn color="secondary" class="on-right" @click="">Chat</q-btn>
+            <q-btn color="secondary" class="on-right" @click="">Perfil</q-btn>
           </q-item-tile>
         </q-item-main>
       </q-item>
+    </q-list>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { Toast, QBtn, QToolbar, QIcon, QToolbarTitle, QField, QInput, QSearch, QItem } from 'quasar'
+import { Toast, QBtn, QToolbar, QIcon, QToolbarTitle, QField, QInput, QSearch, QItem, QList } from 'quasar'
 import axios from 'axios'
 import menu from '../layouts/menu'
 
@@ -35,16 +37,14 @@ export default{
 
   methods: {
     search () {
-      axios.post('search', {
-        name: this.srch
-      })
+      axios.post('search', {name: this.srch})
         .then((response) => {
-          this.results = this.response.data
+          this.results = response.data.users
         }, () => {
           Toast.create.negative('Fallo al listar usuarios')
         })
     }
   },
-  components: { 'q-menu': menu, QBtn, QToolbar, QIcon, QToolbarTitle, QField, QInput, QSearch, QItem }
+  components: { 'q-menu': menu, QBtn, QToolbar, QIcon, QToolbarTitle, QField, QInput, QSearch, QItem, QList }
 }
 </script>
