5920279d24785d328c79a59b0ad103fae575afe6
Unknown <aaronvdp@hotmail.com>
2018-05-08 14:33:41 +0200

    Creada vista de mensajes

---
+++ src/components/messages/messages.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <div class="layout-view">
+      <div class="text-center">
+        <h3>Timeline </h3>
+      </div>
+      <div class="layout-view layout-padding">
+       <q-chat-message v-for="message in messages" :key=message.id
+        name=message.ori
+        :text=message.body
+        v-if="message.ori==this.ori" :v-bind="sent"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import {Toast, QBtn, QToolbar, QIcon, QToolbarTitle, QField, QInput, QChatMessage} from 'quasar'
+  import axios from 'axios'
+  import menu from '../layouts/menu'
+
+  export default{
+    data () {
+      return {
+        messages: [],
+        body: "",
+        ori: "",
+        des: ""
+      }
+    },
+
+    mounted () {
+      this.getMessages()
+    },
+
+    methods: {
+      getMessages () {
+        axios.get('messages',{"des":this.des,"body":this.body})
+          .then((response) => {
+            this.posts = response.data
+          }, () => {
+            Toast.create.negative('Fallo al recuperar mensajes')
+          })
+      },
+      createMessage () {
+          axios.post()
+            .then((response) => {
+                this.getMessages()
+            }, () => {
+                Toast.create.negative('Fallo al enviar mensaje')
+            })
+        
+      }
+    },
+
+    components: {'q-menu': menu, QBtn, QToolbar, QIcon, QToolbarTitle, QField, QInput, QCard, QCardTitle, QCardMain, QCardSeparator, QPullToRefresh}
+  }
+</script>
